0e0ca7a1f0e44d0381a1c64f1b15b0f3edb2c281
Alexander Shishenko <alex@shishenko.com>
2017-07-12 01:36:17 +0300

    Added missing API

--- test_package/example.c
+++ test_package/example.c
@@ -10,3 +10,4 @@ int main(int argc, char** argv) {
 int dht_blacklisted(const struct sockaddr *sa, int salen) {return 0;}
 void dht_hash(void *hash_return, int hash_size, const void *v1, int len1, const void *v2, int len2, const void *v3, int len3) {}
 int dht_random_bytes(void *buf, size_t size) {return 0;}
+int dht_gettimeofday(struct timeval *tv, struct timezone *tz) {return 0;}
